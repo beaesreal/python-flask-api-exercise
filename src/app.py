1b5ffa28108c4c0ba976c3a60e40e7992c30d88d
beaesreal <beaesreal@gmail.com>
2022-11-22 19:35:46 +0000

    Exercise finished

--- src/app.py
+++ src/app.py
@@ -8,6 +8,8 @@ from flask import Flask, jsonify
 # for POST
 from flask import request
 
+# function json.loads
+import json
 
 
 # create a global variable todos
@@ -17,21 +19,33 @@ todos = [
 ]
 
 
+#GET
 @app.route('/todos', methods=['GET'])
 def hello_world():
     json_text = jsonify(todos)
     return json_text
 
 
-
+# POST
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.data
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    decoded_object=json.loads(request_body)
+    todos.append(decoded_object)
+    return jsonify(todos)
+
+
+# DELETE
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ", position)
+    todos.pop(position)
+    return jsonify(todos)
 
 
 
+#------------>>
 # CAUTION!! These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
